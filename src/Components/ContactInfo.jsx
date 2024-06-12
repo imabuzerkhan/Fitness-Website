e6a35a235951f18443d1c956e6a9952fd446278d
imabuzerkhan <abuzerk41@gmail.com>
2024-06-12 16:37:32 +0545

    newcommit

--- src/Components/ContactInfo.jsx
+++ src/Components/ContactInfo.jsx
@@ -7,7 +7,7 @@ const ContactInfo = () => {
   <div className="main-contact-box my-10 md:my-16 px-5 w-full h-auto md:px-30 ">
   <h1 className='text-2xl md:text-4xl font-["Kanit"] font-[800] mb-2 text-center ' > Contact us</h1>
     <p className='text-xl font-["Merriweather"]  text-center ' >Find our way to easily.</p>
-<div className="Contact-Box flex items-center justify-between flex-wrap mt-10 md:mt-20 px-3 md:px-32 ">
+<div className="Contact-Box flex items-center justify-between flex-wrap mt-10 md:mt-20 px-3 md:px-24 ">
 <div className="map">
   <Map></Map>
 </div>
